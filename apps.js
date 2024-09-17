80c170b0cea61d6b76862f0b474816112efc66f3
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-09-17 18:17:24 -0500

    array put into html

--- apps.js
+++ apps.js
@@ -4,3 +4,8 @@ const foods = [
     "Gorditas", 
     "Flautas"
 ] ;
+
+foods.push(" & Chimichangas") ;
+
+document.getElementById("cuisine").innerHTML = "Having spent a majority of my life in New Mexico, I am familiar with great food such as " +
+foods.join(", ") + " etc." ;
