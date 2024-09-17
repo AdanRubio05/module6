4d2c331e7182ce2ff6c04de8c5953c23706d903c
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-09-17 18:25:55 -0500

    Object created

--- apps.js
+++ apps.js
@@ -13,3 +13,8 @@ foods.join(", ") + " etc." ;
 document.getElementById("rant").innerHTML =
 "When moving to central Texas, I was grateful to have knowledge on how to cook these said dishes as I am not a fan of Tex-Mex. Shocker, huh?"
 
+const trek = {
+   city: "Hatch Valley",
+   state: "New Mexico",
+   plant: "Green Chili Peppers",
+}
